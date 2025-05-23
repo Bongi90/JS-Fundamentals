13be7bd6deac78319b90c62d44e97af6cfd43a26
Bongi90 <avalashiya90@gmail.com>
2025-05-23 08:13:53 +0200

    Adds 0-javascript_is_amazing.js script

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+// Create a constant variable named myVar
+const myVar = "JavaScript is amazing"; // Use 'const', not 'var'
+
+// Use console.log() to print the value of myVar
+console.log(myVar); // Use console.log()
